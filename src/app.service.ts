ba1380f838360e7df823e875c1df01415bb4bb3c
Oleksandr Pylypenko <lex.pilipenko@gmail.com>
2021-08-09 16:34:39 +0300

    send metric on root page access

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { StatsD } from 'hot-shots';
+
+const statsdClient = new StatsD({});
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    statsdClient.increment('test_metric', {'foo': 'bar'});
     return 'Hello World!';
   }
 }
